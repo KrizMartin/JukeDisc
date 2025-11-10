5c6b9befb602cc349908728c92b1e077faadc436
Martin Kříž <krizmartin70@gmail.com>
2025-11-10 08:21:54 +0100

    oprava

--- index.php
+++ index.php
@@ -5,8 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>JukeDisc</title>
     <link rel="stylesheet" href="css/style.css">
+    <link rel="shortcut icon" href="images/logo.png" type="image/x-icon">
 </head>
 <body>
+    <div class="menu">
     <table>
         <ul>
             <li><img src="images/menu.svg" alt=""></li>
@@ -15,6 +17,6 @@
             <li><a href="">Přihlásit se</a></li>
         </ul>
     </table>
-    
+    </div>
 </body>
 </html>
